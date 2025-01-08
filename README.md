d749f16a0e19b50de29c14bf3cb6fae670e9cd61
Yui_iuY <changs.yui@mail.ru>
2025-01-08 17:24:58 +0500

    ini

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Fake_News
+
+The task being solved: Detecting fake news. 
+
+Fake news is false information disseminated through social media and other online media to achieve political or ideological goals.
+
+## Libraries
+
+- pandas 
+- matplotlib.pyplot
+- seaborn 
+- sklearn
+
+## Chart
+
+![alt text](https://github.com/YuioiuY/Fake_News/blob/main/source/Figure.png)
+
+### Axes:
+ - Horizontal axis (Predicted): Values predicted by the model.
+ - Vertical axis (Actual): Real values from the test dataset.
+
+## Results:
+
+![alt text](https://github.com/YuioiuY/Fake_News/blob/main/source/img2read/9266.png)
+![alt text](https://github.com/YuioiuY/Fake_News/blob/main/source/img2read/9290.png)
+![alt text](https://github.com/YuioiuY/Fake_News/blob/main/source/img2read/9313.png)
---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+data = pd.read_csv('source/fake_news.csv')
+print(data.head())
+
+X = data['text']
+y = data['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(X_train_tfidf, y_train)
+
+y_pred = pac.predict(X_test_tfidf)
+
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print(f'Accuracy: {accuracy*100:.2f}%')
+print('Confusion Matrix:')
+print(conf_matrix)
+
+
+# Визуализация матрицы ошибок
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', xticklabels=['FAKE', 'REAL'], yticklabels=['FAKE', 'REAL'])
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
